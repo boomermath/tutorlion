cae0bec09fb09835d71befa33e335cc7f1ce737a
boomermath <boomermath13@gmail.com>
2023-09-06 21:33:51 -0400

    make tutors page look better

---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,56 @@
+import Slider from "react-slick";
+import {testimonials} from "../../resources/testimonials.json";
+
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import {useCallback, useState} from "react";
+
+
+const settings = {
+    dots: true,
+    infinite: true,
+    arrows: false,
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 700,
+    responsive: [
+        {
+            breakpoint: 850,
+            settings: {
+                slidesToShow: 1,
+                speed: 500,
+                autoplaySpeed: 3000
+            }
+        },
+    ],
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    swipeToSlide: true,
+};
+
+export default function TestimonialSlider() {
+
+    return (
+        <div className="h-full cursor-ew-resize outline-none">
+            <div>
+                <Slider {...settings}>
+                    {testimonials.map((t) =>
+                        <div key={t.text} className="h-full">
+                            <div
+                                className="bg-white rounded-md hover:-translate-y-2 duration-300 h-[400px] p-4 md:p-10 shadow-lg text-center flex flex-col items-center justify-center m-4">
+                                <p className="text-md md:text-lg m-2 align-middle">
+                                    "{t.text}"
+                                </p>
+                                <p className="m-2 md:m-4 text-sm md:text-md font-semibold">{t.person} <br/> {t.subject}
+                                </p>
+                            </div>
+                        </div>
+                    )}
+                </Slider>
+            </div>
+        </div>
+    )
+}
