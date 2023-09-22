2733aeb791b22e54944e986eeff4c2711468f730
Restyled.io <commits@restyled.io>
2023-09-22 00:38:42 +0000

    Restyled by prettier

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,14 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 const totalMs = 1200;
-export default function Counter({number}) {
-    const [num, setNum] = useState(0);
+export default function Counter({ number }) {
+  const [num, setNum] = useState(0);
 
-    useEffect(() => {
-        if (num < number) {
-            setTimeout(() => setNum(n => ++n), totalMs / number);
-        }
-    }, [num])
+  useEffect(() => {
+    if (num < number) {
+      setTimeout(() => setNum((n) => ++n), totalMs / number);
+    }
+  }, [num]);
 
-    return <span>{num}</span>
+  return <span>{num}</span>;
 }
--- src/components/nav/MobileNav.jsx
+++ src/components/nav/MobileNav.jsx
@@ -1,24 +1,39 @@
-import {useState} from "react";
+import { useState } from "react";
 
+export function MobileNav({ children }) {
+  const [toggled, setToggled] = useState(false);
 
-export function MobileNav({children}) {
-    const [toggled, setToggled] = useState(false);
-
-    return (
-        <>
-            <div className="md:hidden">
-                <div onClick={() => setToggled(t => !t)} className="top-[30px] right-5 fixed z-50 cursor-pointer">
-                    <div
-                        className={`h-1 rounded-md w-8 bg-white m-1 duration-200 ${toggled ? "rotate-45 translate-y-2" : ""}`}></div>
-                    <div className={`h-1 rounded-md w-8 bg-white m-1 ${toggled ? "hidden" : ""}`}></div>
-                    <div className={`h-1 rounded-md w-8 bg-white m-1 duration-200 ${toggled ? "-rotate-45" : ""}`}></div>
-                </div>
-            </div>
-            <div className={`w-screen fixed top-20 shadow-lg bg-brown text-white border-b-2 z-50 border-white ${toggled ? "" : "hidden"}`}>
-                <div className="m-3">
-                    {children}
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="md:hidden">
+        <div
+          onClick={() => setToggled((t) => !t)}
+          className="top-[30px] right-5 fixed z-50 cursor-pointer"
+        >
+          <div
+            className={`h-1 rounded-md w-8 bg-white m-1 duration-200 ${
+              toggled ? "rotate-45 translate-y-2" : ""
+            }`}
+          ></div>
+          <div
+            className={`h-1 rounded-md w-8 bg-white m-1 ${
+              toggled ? "hidden" : ""
+            }`}
+          ></div>
+          <div
+            className={`h-1 rounded-md w-8 bg-white m-1 duration-200 ${
+              toggled ? "-rotate-45" : ""
+            }`}
+          ></div>
+        </div>
+      </div>
+      <div
+        className={`w-screen fixed top-20 shadow-lg bg-brown text-white border-b-2 z-50 border-white ${
+          toggled ? "" : "hidden"
+        }`}
+      >
+        <div className="m-3">{children}</div>
+      </div>
+    </>
+  );
 }
